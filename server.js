03d94ba823dc17ff0508a9cf3b0b76bb0e9b8957
deepak mishra <d.mishra@green-fusion.de>
2022-06-17 15:29:32 +0200

    changes in server

--- server.js
+++ server.js
@@ -3,7 +3,14 @@ import {launchSocketserver} from './utility/socketio.js';
 
 //import NotificationsUpdates from './notificationUpdates';
 
-const server = http.createServer();
+const server = http.createServer((req, res) => {
+  if (req.method == 'GET'){
+    res.end('OKAY');
+    console.log(req.url)
+  }
+});
+
+
 
 launchSocketserver(server);
- server.listen(3000)
+server.listen(3000)
