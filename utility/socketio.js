eaaee4c4c4a410ab699f036a00872636812236b5
deepak mishra <d.mishra@green-fusion.de>
2022-06-26 22:53:41 +0200

    "creating users!"

--- utility/socketio.js
+++ utility/socketio.js
@@ -1,18 +1,16 @@
 import { Server } from "socket.io";
 
 export function launchSocketserver (server){
-
-//Server is a class  object coming from socket.io module or library 
-
+    //Server is a class  object coming from socket.io module or library
     const io = new Server();
+    // io.of('/trialroute').on('connection', (socket) => {
+    //     socket.on('conn',data => {
+    //       console.log("DATA:", data)})
+    //       } )
 
-io.of('/trialroute').on('connection', (socket) => {
-    
-    socket.on('conn',data => {
-      console.log("DATA:", data)})
-      } )
-    io.attach(server)
+    io.attach(server);
     let users = [];
+
     //Some tutorials may also mention io.sockets, it's simply an alias for io.of("/").
     // io.on("connection", (socket) => {});
     // io.use((socket, next) => { next() });
@@ -21,33 +19,25 @@ io.of('/trialroute').on('connection', (socket) => {
     // io.of("/").on("connection", (socket) => {});
     // io.of("/").use((socket, next) => { next() });
     // io.of("/").emit("hello")
-    io.on('connection', function (socket) {
+    //The Server instance emits one single event: "connection"
+    //Well, technically two, but connect is an alias for connection
+    //the connection, connect and new_namespace strings are reserved and cannot be used in your application.
+
+    io.sockets.on('connection', function (socket) {
         socket.on("Join!", username => {
+            try {
             const user = {
                 username,
                 id: socket.id,
             }
             users.push(user);
             io.emit("New User", users)
-        });
        
-        
-            socket.emit('LISTEN', "My Name Is DASHANAN!")
-            //here after
-            socket.on('ALARMNOTIFICATION', async function (data) {
-            try {
-                // console.log(socket)
-                // console.log(data)
             } catch (err) {
                 console.log(err);
             }
-            });
-            const count = io.engine.clientsCount;
-            //const cli1 = socket.
-            console.log(count);
-        }
-        );
-        
-       
+        });
+    });          
+
 }
 
